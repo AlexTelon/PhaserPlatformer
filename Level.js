1fd518100f8eed25f7091c2c5e4811bf4b34a06f
AlexTelon <alex.telon@gmail.com>
2014-10-01 22:33:27 +0200

    in the middle of getting coin collecting to work

--- Level.js
+++ Level.js
@@ -16,28 +16,57 @@ Level.prototype = {
 	preload: function() {
 		game.load.tilemap('map', 'levels.json', null, Phaser.Tilemap.TILED_JSON);
 		game.load.image('ground_1x1', 'assets/tilemaps/tiles/ground_1x1.png');
-		game.load.image('coin', 'assets/sprites/coin.png');
+		game.load.spritesheet('coin', 'assets/sprites/coin.png', 32, 32);
 	},
 
 	create: function() {
 		game.physics.startSystem(Phaser.Physics.P2JS);
 
-		//Setup both the world and the upside down world
+		// so we get collision callbacks
+		this.game.physics.p2.setImpactEvents(true);
+
+		//Setup both the different levels
 		this.game.stage.backgroundColor = '#2d2d2d';
 		this.map = this.game.add.tilemap('map');
 
 		this.map.addTilesetImage('ground_1x1');
 		this.map.addTilesetImage('coin');
 
-		this.layer[1] = this.map.createLayer('level3UD');
+		this.layer[1] = this.map.createLayer('level2UD');
 		this.layer[1].resizeWorld();
 
-		this.layer[0] = this.map.createLayer('level1');
+		this.layer[0] = this.map.createLayer('level2');
 		this.layer[0].resizeWorld();
 
 		this.layer[0].alpha = 1;
 		this.layer[1].alpha = 0.3;
 		this.currentLayer = this.layer[0];
+
+		//  Set the tiles for collision.
+		//  Do this BEFORE generating the p2 bodies below.
+		this.map.setCollisionBetween(1, 12, true, this.currentLayer, true);
+
+		//  Convert the tilemap layer into bodies. Only tiles that collide (see above) are created.
+		//  This call returns an array of body objects which you can perform addition actions on if
+		//  required. There is also a parameter to control optimising the map build.
+		this.mapObjects = this.game.physics.p2.convertTilemap(this.map, this.currentLayer);
+
+		// Setup all collisions
+		this.mapCollisionGroup = this.game.physics.p2.createCollisionGroup();
+		this.coinCollisionGroup = this.game.physics.p2.createCollisionGroup();
+		this.playerCollisionGroup = this.game.physics.p2.createCollisionGroup();
+		this.game.physics.p2.updateBoundsCollisionGroup();
+
+		this.mapGroup = this.game.add.group();
+		this.mapGroup.enableBody = true;
+		this.mapGroup.physicsBodyType = Phaser.Physics.P2JS;
+		// Fortsätt här och försök efterfölja testStuff.html och hur de gör med pandorna och spelaren. Har ju bara missat
+		// nåt litet här..
+		this.mapGroup.
+
+		this.mapObjects.forEach(function(body) {
+			body.setCollisionGroup(this.mapCollisionGroup);
+		}, this);
 
 		game.physics.p2.restitution = 0;
 		game.physics.p2.gravity.y = 500;
@@ -51,29 +80,24 @@ Level.prototype = {
 	setPlayer: function(player) {
 		this.player = player;
 		this.player.setStartPos(300,200);
-	this.player.body.createBodyCallback();
-		console.log(this.game.physics);
-		//this.game.physics.collide(this.player, this.map);
-		// setup overlap detection between the coin tile and player
-		// this.player as a callback context might be wrong, only this instead?
-		this.map.setTileLocationCallback(45,2,1,1, this.player.collectCoin, this.player, this.layer[0]);
-		this.map.setTileLocationCallback(1,10,2,2, this.player.collectCoin, this.player, this.layer[0]);
-		this.map.setTileLocationCallback(45,15,2,2, this.player.collectCoin, this.player, this.layer[1]);
-		this.map.setTileLocationCallback(1,7,1,1, this.player.collectCoin, this.player, this.layer[1]);
-		console.log("the location callback is in place")
-		console.log(this.map.getTile(1,10,this.layer[0]));
-		console.log("now we MUST figure out how these callbacks are to be triggerd");
+		this.coinGroup = this.game.add.group();
+		this.coinGroup.enableBody = true;
+		//this.coinGroup.physicsBodyType = Phaser.Physics.P2JS;
 
-		//  Set the tiles for collision.
-		//  Do this BEFORE generating the p2 bodies below.
-		this.map.setCollisionBetween(1, 12, true, this.currentLayer, true);
+		// Använd nedanstående grej för att skapa coin sprites och gör sedan normala collisioner med dem! :)
+		this.map.createFromObjects('level2OBJ', 26, 'coin', 0, true, false, this.coinGroup);
 
-		//  Convert the tilemap layer into bodies. Only tiles that collide (see above) are created.
-		//  This call returns an array of body objects which you can perform addition actions on if
-		//  required. There is also a parameter to control optimising the map build.
-		this.mapObjects = this.game.physics.p2.convertTilemap(this.map, this.currentLayer);
+		//  Add animations to all of the coin sprites
+		this.coinGroup.callAll('animations.add', 'animations', 'spin', [0, 1, 2, 3, 4, 5], 10, true);
+		this.coinGroup.callAll('animations.play', 'animations', 'spin');
 
-		this.game.physics.p2.setImpactEvents(true);
+
+		console.log(this.coinGroup.children[0].body);
+		console.log(this.coinCollisionGroup);
+		this.player.player.body.setCollisionGroup(this.playerCollisionGroup);
+		this.coinGroup.children[0].body.setCollisionGroup(this.coinCollisionGroup);
+		this.player.player.body.collides(this.coinCollisionGroup, this.player.collectCoin);
+		this.player.player.body.collides([this.mapCollisionGroup, this.playerCollisionGroup]);
 	},
 
 	update: function() {
@@ -90,7 +114,6 @@ Level.prototype = {
 
 		// TODO this probably is the bottleneck, flipping the map makes the game REALLY slow after a while.
 		this.map = this.game.add.tilemap('map');
-		console.log(this.map.game);
 		this.map.addTilesetImage('ground_1x1');
 		if(this.flipSwitch) {
 			this.currentLayer = this.layer[0];
