15c3dce3fd6450442cedb25e0cd77c31c8bf42a6
AlexTelon <alex.telon@gmail.com>
2014-10-09 17:08:00 +0200

    changing level now works, performance issue when swapping too many times fixed

--- Level.js
+++ Level.js
@@ -7,14 +7,16 @@ Level = function(game, player) {
 	this.layer = [];
 	this.currentLayer = null;
 	this.flipSwitch = false;
-
+	this.collectedCoins = 0;
+	this.currentLevel = 1;
+	this.changeOfLevel = false;
 	this.mapObjects = null;
 };
 
 Level.prototype = {
 
 	preload: function() {
-		game.load.tilemap('map', 'levels.json', null, Phaser.Tilemap.TILED_JSON);
+		game.load.tilemap('map', 'levels2.json', null, Phaser.Tilemap.TILED_JSON);
 		game.load.image('ground_1x1', 'assets/tilemaps/tiles/ground_1x1.png');
 		game.load.spritesheet('coin', 'assets/sprites/coin.png', 32, 32);
 
@@ -44,58 +46,19 @@ Level.prototype = {
 
 		this.player.sprite.body.setCollisionGroup(this.playerCollisionGroup);
 
-		this.player.setStartPos(100,200);
-
-		//Setup the different levels
-
-		this.game.stage.backgroundColor = '#2d2d2d';
-		this.sky = game.add.tileSprite(0, 0, 800, 600, 'sky');
-		this.sky.fixedToCamera = true;
-		this.map = this.game.add.tilemap('map');
-
-		this.map.addTilesetImage('ground_1x1');
-		this.map.addTilesetImage('coin');
-
-		this.layer[1] = this.map.createLayer('level2UD');
-		this.layer[1].resizeWorld();
-
-		this.layer[0] = this.map.createLayer('level2');
-		this.layer[0].resizeWorld();
-
-		this.layer[0].alpha = 1;
-		this.layer[1].alpha = 0.3;
-		this.currentLayer = this.layer[0];
-
-		//  Set the tiles for collision.
-		//  Do this BEFORE generating the p2 bodies below.
-		this.map.setCollisionBetween(1, 12, true, this.currentLayer, true);
-
-		//  Convert the tilemap layer into bodies. Only tiles that collide (see above) are created.
-		//  This call returns an array of body objects which you can perform addition actions on if
-		//  required. There is also a parameter to control optimising the map build.
-		this.mapObjects = this.game.physics.p2.convertTilemap(this.map, this.currentLayer);
-
-		this.mapCollisionGroup = this.game.physics.p2.createCollisionGroup();
-
-		this.mapObjects.forEach(function(body) {
-			body.setCollisionGroup(this.mapCollisionGroup);
-			body.collides([this.mapCollisionGroup, this.playerCollisionGroup]);
-		}, this);
-
-		this.player.sprite.body.collides(this.mapCollisionGroup);
-
 		this.coinGroup = this.game.add.group();
 		this.coinGroup.enableBody = true;
 		this.coinGroup.physicsBodyType = Phaser.Physics.P2JS;
 
-		// Använd nedanstående grej för att skapa coin sprites och gör sedan normala collisioner med dem! :)
-		this.map.createFromObjects('level2OBJ', 26, 'coin', 0, true, false, this.coinGroup);
 
-		for (var i = 0; i < this.coinGroup.length; i++) {
-			var coin = this.coinGroup.getAt(i).body;
-			coin.setCollisionGroup(this.mapCollisionGroup);
-			coin.static = true;
-		}
+		//	this.sky = game.add.tileSprite(0, 0, 800, 600, 'sky');
+		//	this.sky.fixedToCamera = true;
+		this.map = this.game.add.tilemap('map');
+		this.map.addTilesetImage('ground_1x1');
+		this.map.addTilesetImage('coin');
+
+		//Setup the different levels
+		this.setupLevel();
 
 		//  Add animations to all of the coin sprites
 		this.coinGroup.callAll('animations.add', 'animations', 'spin', [0, 1, 2, 3, 4, 5], 10, true);
@@ -105,7 +68,7 @@ Level.prototype = {
 
 		this.coinSound = this.game.add.audio('coin');
 		this.music = this.game.add.audio('music');
-		this.music.play('',0,0.5,true);
+		this.music.play('',0,0.0,true);
 
 		this.playerGroup = game.add.group();
 		this.playerGroup.add(this.player.sprite);
@@ -117,6 +80,10 @@ Level.prototype = {
 	},
 
 	update: function() {
+		if(this.changeOfLevel) {
+			this.changeOfLevel = false;
+			this.setupLevel();
+		}
 	},
 
 	setUpMapSwitch: function() {
@@ -126,7 +93,14 @@ Level.prototype = {
 			obj.clearShapes();
 		});
 
-		this.game.stage.backgroundColor = '#2d2d2d';
+		if (this.mapObjects !== null) {
+			this.mapObjects.forEach(function (body) {
+				console.log(body);
+				body.remove;
+				body.removeFromWorld();
+			});
+		}
+
 
 		// TODO this probably is the bottleneck, flipping the map makes the game REALLY slow after a while.
 		this.map = this.game.add.tilemap('map');
@@ -157,7 +131,85 @@ Level.prototype = {
 			body.setCollisionGroup(this.mapCollisionGroup);
 			body.collides([this.mapCollisionGroup, this.playerCollisionGroup]);
 		}, this);
+	},
+
+	setupLevel: function() {
+
+		if (this.mapObjects !== null) {
+			this.mapObjects.forEach(function (body) {
+				console.log(body);
+				body.remove;
+				body.removeFromWorld();
+			});
+		}
+
+		console.log(this.layer.length);
+		if(this.layer.length != 0) {
+			this.layer[0].alpha = 0;
+			this.layer[1].alpha = 0;
+		}
+
+		console.log(this.mapObjects);
+
+		this.map.destroy();
+		this.map = this.game.add.tilemap('map');
+		this.map.addTilesetImage('ground_1x1');
+		this.map.addTilesetImage('coin');
+
+		switch(this.currentLevel){
+			// Använd nedanstående grej för att skapa coin sprites och gör sedan normala collisioner med dem! :)
+			case 1:
+				this.layer[0] = this.map.createLayer('level1');
+				this.layer[1] = this.map.createLayer('level1UD');
+				this.map.createFromObjects('level1OBJ', 26, 'coin', 0, true, false, this.coinGroup);
+				break;
+			case 2:
+				this.layer[0] = this.map.createLayer('level2');
+				this.layer[1] = this.map.createLayer('level2UD');
+				this.map.createFromObjects('level2OBJ', 26, 'coin', 0, true, false, this.coinGroup);
+				break;
+			case 3:
+				this.layer[0] = this.map.createLayer('level3');
+				this.layer[1] = this.map.createLayer('level3UD');
+				this.map.createFromObjects('level3OBJ', 26, 'coin', 0, true, false, this.coinGroup);
+				break;
+
+		}
+
+		this.layer[1].resizeWorld();
+		this.layer[0].resizeWorld();
+
+		this.layer[0].alpha = 1;
+		this.layer[1].alpha = 0.3;
+		this.currentLayer = this.layer[0];
+
+		//  Set the tiles for collision.
+		//  Do this BEFORE generating the p2 bodies below.
+		this.map.setCollisionBetween(1, 12, true, this.currentLayer, true);
+
+		//  Convert the tilemap layer into bodies. Only tiles that collide (see above) are created.
+		//  This call returns an array of body objects which you can perform addition actions on if
+		//  required. There is also a parameter to control optimising the map build.
+		this.mapObjects = this.game.physics.p2.convertTilemap(this.map, this.currentLayer);
+
+		this.mapCollisionGroup = this.game.physics.p2.createCollisionGroup();
+
+		this.mapObjects.forEach(function(body) {
+			body.setCollisionGroup(this.mapCollisionGroup);
+			body.collides([this.mapCollisionGroup, this.playerCollisionGroup]);
+		}, this);
+
+		this.player.sprite.body.collides(this.mapCollisionGroup);
+
+		for (var i = 0; i < this.coinGroup.length; i++) {
+			var coin = this.coinGroup.getAt(i).body;
+			coin.setCollisionGroup(this.mapCollisionGroup);
+			coin.static = true;
+		}
+
 	}
+
+
 };
 
 function checkCollision(body1, body2) {
@@ -178,6 +230,13 @@ function checkCollision(body1, body2) {
 		if ((body1.sprite.name === 'player' && body2.sprite.name === 'coin' ) || (body2.sprite.name === 'player' && body1.sprite.name === 'coin')) {
 			this.player.collectCoin();
 			this.coinSound.play();
+			this.collectedCoins++;
+			console.log(this.collectedCoins);
+			if(this.collectedCoins == 2) {
+				this.collectedCoins = 0;
+				this.currentLevel++;
+				this.changeOfLevel = true;
+			}
 
 			if (body2.sprite.name === 'coin') {
 				body2.sprite.kill();
@@ -188,5 +247,5 @@ function checkCollision(body1, body2) {
 		}
 	}
 	return true;
-
 }
+
